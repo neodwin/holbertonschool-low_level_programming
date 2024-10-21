9cd94f64392000735161d1933c3e7e60611881fa
neodwin <edwin.dervaux@gmail.com>
2024-10-21 15:25:02 +0200

    Add 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *  main - using printf
+ *
+ * Description: bla
+ * Return: always return 0 to indicate success
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+printf("Size of char: %lu bytes\n", sizeof(f));
+printf("Size of int: %lu bytes\n", sizeof(a));
+printf("Size of float: %lu bytes\n", sizeof(f));
+printf("Size of long int: %lu bytes\n", sizeof(b));
+printf("Size of long long int: %lu bytes\n", sizeof(C));
+return (0);
+}
