b88510ab81a0d9c6ba0634d7575eb51cfb7c3ad9
neodwin <edwin.dervaux@gmail.com>
2024-10-21 14:46:27 +0200

    Add 5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - using puts
+ *
+ * Description: bla
+ * Return: always return 0 to indicate success
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art");
+return (0);
+}
