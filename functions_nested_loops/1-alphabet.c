ff9461bdeaca232f4ebf546e67c2d6f43920adf0
neodwin <edwin.dervaux@gmail.com>
2024-10-23 16:33:44 +0200

    Add 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{	
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+}
