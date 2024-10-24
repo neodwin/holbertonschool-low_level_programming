ef554c45382e9922490c20229cfce613821a248c
neodwin <edwin.dervaux@gmail.com>
2024-10-24 10:05:13 +0200

    Add 3-is_lower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ * @c: check the character
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
