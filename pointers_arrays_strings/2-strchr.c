d3427432ea0a33df0e66b6d5370f42df9d526d4e
neodwin <edwin.dervaux@gmail.com>
2024-11-04 15:11:00 +0100

    Add 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: pointer to the first occurrence of the character
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (s[i] == c)
+		return (s + i);
+	return (0);
+}
