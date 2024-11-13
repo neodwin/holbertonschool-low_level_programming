2ac4b77c5e21958f3dfb4238085ae30b91e2196a
neodwin <edwin.dervaux@gmail.com>
2024-11-13 17:26:41 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,30 +12,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, j, k;
 	dog_t *ptr;
 
-	j = k = 0;
-	while (name[j++])
-		;
-	while (owner[k++])
-		;
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
-		return (NULL);
 
-	ptr->name = malloc(j * sizeof(ptr->name));
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
-	for (i = 0; i < j; i++)
-		ptr->name[i] = name[i];
+	}
 
+	ptr->name = malloc(sizeof(name) + 1);
+	ptr->owner = malloc(sizeof(owner) + 1);
+
+	if (ptr->name == NULL || ptr->owner == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr->name = name;
+	ptr->owner = owner;
 	ptr->age = age;
-
-	ptr->owner = malloc(k * sizeof(ptr->owner));
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i < k; i++)
-		ptr->owner[i] = owner[i];
 	return (ptr);
 }
