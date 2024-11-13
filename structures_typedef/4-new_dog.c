e72a967e055acd9b396bd3ad8e601f4d51621ee1
neodwin <edwin.dervaux@gmail.com>
2024-11-13 18:06:20 +0100

    update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -39,8 +39,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < k; i++)
 		ptr->owner[i] = owner[i];
 
-	strcpy(ptr->name, name);
-	strcpy(ptr->owner, owner);
-
 	return (ptr);
 }
