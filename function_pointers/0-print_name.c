a9707213e44f605531d565e2c3014483806de215
neodwin <edwin.dervaux@gmail.com>
2024-11-18 10:20:54 +0100

    Add 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - print name
+  * @name: input name
+  * @f: function pointer
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
