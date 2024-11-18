680e6b71b349357b222daf0685148476fe92d1f5
neodwin <edwin.dervaux@gmail.com>
2024-11-18 14:58:07 +0100

    fix bug header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_POINTER_H
+#ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
@@ -8,4 +8,4 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* FUNCTION_POINTER_H */
+#endif /* FUNCTION_POINTERS_H */
