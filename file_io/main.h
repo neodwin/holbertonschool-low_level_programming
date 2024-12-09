349de74b1be2ea8d80fedb4837ab8988de7929bf
neodwin <edwin.dervaux@gmail.com>
2024-12-09 16:11:29 +0100

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -18,6 +18,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void error_exit(const char *message, int code, const char *file);
-int main(int argc, char *argv[]);
+int main(void);
 
 #endif /* MAIN_H */
